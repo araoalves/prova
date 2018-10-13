b8b674f87a1c12accb7c69bb4834c7b93bde0c70
Christian Oliveira <christianoliveirati@gmail.com>
2018-10-13 13:34:53 -0300

    template da tela de login

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -3,11 +3,11 @@ from app import app
 
 
 @app.route("/")
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
 @app.route("/index")
 def index():
     return render_template('index.html')
-
-
-@app.route("/login")
-def login():
-    return render_template('base.html')
